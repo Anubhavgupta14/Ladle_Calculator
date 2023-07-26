fec94d10e1b6501c5a0c4498ba56a14e892161b7
Anubhavgupta14 <anubhavgu2002@gmail.com>
2023-07-26 23:01:01 +0530

    First Commit

---
+++ app/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <h1>
+      Ladle Calculator
+    </h1>
+  )
+}
+
+export default Header
--- app/page.js
+++ app/page.js
@@ -1,95 +1,102 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client"
+import * as React from "react";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import Header from "./components/Header";
+import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
+import { useState } from "react";
 
-export default function Home() {
+export default function BasicTextFields() {
+
+  const [topdiameter, setTopdiameter] = useState(null);
+  const [bottomdiameter, setBottomdiameter] = useState(null);
+  const [height, setHeight] = useState(null);
+  const [bottomlining, setBottomlining] = useState(null);
+  const [sidelining, setSidelining] = useState(null);
+  const [freeboard, setFreeboard] = useState(null);
+  const [pieHby3, setPieHby3] = useState(null);
+
+  const result = ()=>{
+    const inbetweenheight = height-bottomlining-freeboard
+    const pieHby3 = (3.1416/3000)*inbetweenheight
+    console.log(pieHby3)
+    setPieHby3(pieHby3.toFixed(3))
+  }
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <Box
+      component="form"
+    >
+      <Header />
+      <div className="content">
+        <div className="row">
+          <TextField
+            required
+            className="textfield"
+            id="outlined-number"
+            label="Top Diameter"
+            variant="outlined"
+            type="number"
+            onChange={(e) => setTopdiameter(e.target.value)}
+          />
+          <TextField
+          required
+          className="textfield"
+            id="outlined-number"
+            label="Bottom Diameter"
+            variant="outlined"
+            type="number"
+            onChange={(e) => setBottomdiameter(e.target.value)}
+          />
+        </div>
+        <div className="row">
+          <TextField
+          required
+          className="textfield"
+            id="outlined-number"
+            label="Height"
+            variant="outlined"
+            type="number"
+            onChange={(e) => setHeight(e.target.value)}
+          />
+          <TextField
+          required
+          className="textfield"
+            id="outlined-number"
+            label="Bottom Lining"
+            variant="outlined"
+            type="number"
+            onChange={(e) => setBottomlining(e.target.value)}
+          />
+        </div>
+        <div className="row">
+          <TextField
+          required
+          className="textfield"
+            id="outlined-number"
+            label="Side Lining"
+            variant="outlined"
+            type="number"
+            onChange={(e) => setSidelining(e.target.value)}
+          />
+          <TextField
+          required
+          className="textfield"
+            id="outlined-number"
+            label="Free Board"
+            variant="outlined"
+            type="number"
+            onChange={(e) => setFreeboard(e.target.value)}
+          />
         </div>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+      <Stack spacing={2} direction="row">
+        <Button variant="contained" color="primary" onClick={()=>{result()}}>
+          Calculate
+        </Button>
+      </Stack>
+      <div style={{ fontSize: "20px" }}>${pieHby3}</div>
+    </Box>
+  );
 }
