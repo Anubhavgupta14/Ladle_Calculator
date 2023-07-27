0879cc4dcebe94b6d3c8ecbcc5d7b2c0a17ea391
Anubhavgupta14 <anubhavgu2002@gmail.com>
2023-07-27 23:23:48 +0530

    After Lining Parameters added

--- app/page.js
+++ app/page.js
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import * as React from "react";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
@@ -8,7 +8,6 @@ import Button from "@mui/material/Button";
 import { useState } from "react";
 
 export default function BasicTextFields() {
-
   const [topdiameter, setTopdiameter] = useState(null);
   const [bottomdiameter, setBottomdiameter] = useState(null);
   const [height, setHeight] = useState(null);
@@ -16,20 +15,31 @@ export default function BasicTextFields() {
   const [sidelining, setSidelining] = useState(null);
   const [freeboard, setFreeboard] = useState(null);
   const [pieHby3, setPieHby3] = useState(null);
+  const [freeboardleveldiameter, setFreeboardleveldiameter] = useState(0);
+  const [bottomliningleveldiameter, setBottomliningleveldiameter] = useState(0);
+  const [inbetweenheight, setInbetweenheight] = useState(0);
 
-  const result = ()=>{
-    const inbetweenheight = height-bottomlining-freeboard
-    const pieHby3 = (3.1416/3000)*inbetweenheight
-    console.log(pieHby3)
-    setPieHby3(pieHby3.toFixed(3))
-  }
+  const result = () => {
+    const inbetweenheight = height - bottomlining - freeboard;
+    setInbetweenheight(inbetweenheight)
+    const pieHby3 = (3.1416 / 3000) * inbetweenheight;
+    // console.log(pieHby3);
+    setPieHby3(pieHby3.toFixed(3));
+
+    const I14 =(0.5 * (topdiameter - bottomdiameter)) / inbetweenheight;
+    const freeboardleveldiameter = topdiameter - 2 * sidelining - I14 * 2 * freeboard;
+    setFreeboardleveldiameter(Math.round(freeboardleveldiameter));
+    // console.log(freeboardleveldiameter)
+
+    const bottomliningleveldiameter = (I14*2*bottomlining)+(bottomdiameter-2*sidelining)
+    setBottomliningleveldiameter(Math.round(bottomliningleveldiameter))
+  };
 
   return (
-    <Box
-      component="form"
-    >
+    <Box component="form">
       <Header />
       <div className="content">
+        <h2>Fabricated Dimensions</h2>
         <div className="row">
           <TextField
             required
@@ -41,8 +51,8 @@ export default function BasicTextFields() {
             onChange={(e) => setTopdiameter(e.target.value)}
           />
           <TextField
-          required
-          className="textfield"
+            required
+            className="textfield"
             id="outlined-number"
             label="Bottom Diameter"
             variant="outlined"
@@ -52,8 +62,8 @@ export default function BasicTextFields() {
         </div>
         <div className="row">
           <TextField
-          required
-          className="textfield"
+            required
+            className="textfield"
             id="outlined-number"
             label="Height"
             variant="outlined"
@@ -61,8 +71,8 @@ export default function BasicTextFields() {
             onChange={(e) => setHeight(e.target.value)}
           />
           <TextField
-          required
-          className="textfield"
+            required
+            className="textfield"
             id="outlined-number"
             label="Bottom Lining"
             variant="outlined"
@@ -72,8 +82,8 @@ export default function BasicTextFields() {
         </div>
         <div className="row">
           <TextField
-          required
-          className="textfield"
+            required
+            className="textfield"
             id="outlined-number"
             label="Side Lining"
             variant="outlined"
@@ -81,8 +91,8 @@ export default function BasicTextFields() {
             onChange={(e) => setSidelining(e.target.value)}
           />
           <TextField
-          required
-          className="textfield"
+            required
+            className="textfield"
             id="outlined-number"
             label="Free Board"
             variant="outlined"
@@ -92,11 +102,25 @@ export default function BasicTextFields() {
         </div>
       </div>
       <Stack spacing={2} direction="row">
-        <Button variant="contained" color="primary" onClick={()=>{result()}}>
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={() => {
+            result();
+          }}
+        >
           Calculate
         </Button>
       </Stack>
-      <div style={{ fontSize: "20px" }}>${pieHby3}</div>
+      <div className="outcome">
+        <p>&prod;H/3: {pieHby3}</p>
+        <p>R: {freeboardleveldiameter/2}</p>
+        <p>r: {bottomliningleveldiameter/2}</p>
+        <p>R^2+Rr+r^2: {((freeboardleveldiameter/2)*(freeboardleveldiameter/2))+((bottomliningleveldiameter/2)*(bottomliningleveldiameter/2))+(((bottomliningleveldiameter/2))*(freeboardleveldiameter/2))/1000000}</p>
+        <p>Free Board Level Diameter : {freeboardleveldiameter}</p>
+        <p>Bottom Lining Level Diameter : {bottomliningleveldiameter}</p>
+        <p>In Between Heigh : {inbetweenheight}</p>
+      </div>
     </Box>
   );
 }
