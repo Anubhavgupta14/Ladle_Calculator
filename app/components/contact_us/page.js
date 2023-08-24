2a439e201eb199a47cb001dd7d810285d418dbc4
Anubhavgupta14 <anubhavgu2002@gmail.com>
2023-08-25 00:18:23 +0530

    Contact page fixed

--- app/components/contact_us/page.js
+++ app/components/contact_us/page.js
@@ -1,43 +1,79 @@
 "use client";
 import { useState } from "react";
-import TextField from "@mui/material/TextField";
 import "./globals.css";
-import { useMediaQuery } from "@mui/material";
-import { useTheme } from "@mui/material/styles";
 import Navbar from "../navbar/page";
 
-
 const page = () => {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [phone, setPhone] = useState("");
-  const [comment, setComment] = useState("");
-  const theme = useTheme();
-  const isMobile = useMediaQuery(theme.breakpoints.up("sm"));
-  return (
-    <>
-    <Navbar/>
-    <div className="contact_body">
-      <h2 className="head">Contact Us</h2>
-      <div className="details">
-        <div className="contact_data">
-          <span className="detail_head">Street:</span> 39/42, Modi Street, Fort
-        </div>
-        <div className="contact_data">
-          <span className="detail_head">City:</span> Mumbai
-        </div>
-        <div className="contact_data">
-          <span className="detail_head">State/province/area:</span> Maharastra
-        </div>
-        <div className="contact_data">
-          <span className="detail_head">Country calling code:</span> +91
-        </div>
-        <div className="contact_data">
-          <span className="detail_head">Country:</span> India
-        </div>
-      </div>
+  const [isEmailValid, setIsEmailValid] = useState(true);
 
-      <div className="form">
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    phone: "",
+    comments: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+
+    if (name === "phone") {
+      // Allow only numbers in phone input
+      const numericValue = value.replace(/\D/g, "");
+      setFormData((prevData) => ({
+        ...prevData,
+        [name]: numericValue,
+      }));
+    } else if (name === "email") {
+      // Basic email validation
+      const isValidEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value);
+      setIsEmailValid(isValidEmail || value === ""); // Update email validity
+      setFormData((prevData) => ({
+        ...prevData,
+        [name]: value,
+      }));
+    } else {
+      setFormData((prevData) => ({
+        ...prevData,
+        [name]: value,
+      }));
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // You can perform form submission or data handling here
+    console.log("Form data submitted:", formData);
+  };
+  return (
+    <div>
+      <Navbar />
+
+      <div className="contact_body">
+        <div className="container">
+          <div>
+            <h2 className="heading">Contact Us</h2>
+            <div className="details">
+              <div className="contact_data">
+                <span className="detail_head">Street:</span> 39/42, Modi Street,
+                Fort
+              </div>
+              <div className="contact_data">
+                <span className="detail_head">City:</span> Mumbai
+              </div>
+              <div className="contact_data">
+                <span className="detail_head">State/province/area:</span>{" "}
+                Maharastra
+              </div>
+              <div className="contact_data">
+                <span className="detail_head">Country calling code:</span> +91
+              </div>
+              <div className="contact_data">
+                <span className="detail_head">Country:</span> India
+              </div>
+            </div>
+          </div>
+
+          {/* <div className="form">
         <div className="row">
           <TextField
             sx={{ m: 1, minWidth: isMobile ? 700 : 135 }}
@@ -86,9 +122,59 @@ const page = () => {
                 Send
             </button>
         </div>
+      </div> */}
+
+          <form onSubmit={handleSubmit}>
+            <div>
+              <label htmlFor="name">Name:</label>
+              <input
+                type="text"
+                id="name"
+                name="name"
+                value={formData.name}
+                onChange={handleChange}
+              />
+            </div>
+            <div>
+              <label
+                htmlFor="email"
+                className={isEmailValid ? "" : "error-label"}
+              >
+              Email: {isEmailValid ? "" : "Invalid Email"}
+              </label>
+              <input
+                type="email"
+                id="email"
+                name="email"
+                value={formData.email}
+                required
+                onChange={handleChange}
+              />
+            </div>
+            <div>
+              <label htmlFor="phone">Phone No.:</label>
+              <input
+                type="tel"
+                id="phone"
+                name="phone"
+                value={formData.phone}
+                onChange={handleChange}
+              />
+            </div>
+            <div>
+              <label htmlFor="comments">Comments:</label>
+              <textarea
+                id="comments"
+                name="comments"
+                value={formData.comments}
+                onChange={handleChange}
+              />
+            </div>
+            <button type="submit">Submit</button>
+          </form>
+        </div>
       </div>
     </div>
-    </>
   );
 };
 
